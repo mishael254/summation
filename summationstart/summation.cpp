a1217bf03951d9043c3fdc7bb1400f2d02861613
mishael254 <mishaelmishantoz@gmail.com>
2022-06-10 16:09:02 +0300

    create an if else statement and with it pass the parameters if value 1 is less than value 2 then it should show that value 1 is less than value 2 and also output sum subtraction multiplication and division of both values

--- summationstart/summation.cpp
+++ summationstart/summation.cpp
@@ -14,7 +14,7 @@ int main() {
 		int tsub = val1 - val2;
 		int tdiv = val1 / val2;
 		if (val1 > val2) {
-			cout << "Value one is greater than value two\nWhich can be also said as.., value two is less than value one\n" << "\nThe sum is \n" << tsum << "\nMultiplication is \n" << tmul << "\nsubtraction is\n" << tsub << "\nDivision is \n" << tdiv;
+			cout << "Value one is greater than value two\nWhich can be also said as.., value two is less than value one\n" << "\nThe sum is \n" << tsum  << "\nMultiplication is \n" << tmul << "\nsubtraction is\n" << tsub << "\nDivision is \n" << tdiv;
 
 		}
 		else if (val1 < val2) {
